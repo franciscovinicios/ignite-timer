da61278e4bfe66873ce49db237cba16dc9f601fb
franciscovinicios <francisco.viniciosti@gmail.com>
2022-07-10 10:59:55 -0300

    Pages and routes

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,60 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Historico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Task 1</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 2</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 3</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Closed</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Task 4</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,90 @@
+import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+const newCicleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'write a task'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'Cycle needs at least 5 minutes')
+    .max(60, 'Cycle needs max 60 minutes'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCicleFormValidationSchema>
+
+export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCicleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNeCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form action="" onSubmit={handleSubmit(handleCreateNeCycle)}>
+        <FormContainer>
+          <label htmlFor="task">I will work in</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Name for your project"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">duration</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
